ec4ecb8f702aeaa5eabe0a4c8cdf8d37a008721b
adamyulian <aadamyulian@Gmail.com>
2024-04-04 13:22:22 +0700

    oke

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
 <body class="bg-gray-100 flex items-center justify-center h-screen">
     <div class="text-center">
         <h1 class="text-4xl font-bold mb-4">Selamat Datang di Halaman Pengamanan Kendaraan Lebaran 2024 <br> Pemerintah Kota Surabaya</h1>
-        <a href="/admin/login" class="bg-blue-500 text-white font-bold py-2 px-4 rounded-full hover:bg-blue-700">MASUK DISINI</a>
+        <a href="/admin/login" class="bg-blue-500 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-700">MASUK DISINI</a>
     </div>
 </body>
 </html>
