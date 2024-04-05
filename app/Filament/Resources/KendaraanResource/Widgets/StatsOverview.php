7c9c5d285863e1201fd90baa4c3549c0791aef83
adamyulian <aadamyulian@Gmail.com>
2024-04-05 13:56:27 +0700

    sassaa

--- app/Filament/Resources/KendaraanResource/Widgets/StatsOverview.php
+++ app/Filament/Resources/KendaraanResource/Widgets/StatsOverview.php
@@ -9,6 +9,8 @@ use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 
 class StatsOverview extends BaseWidget
 {
+    protected static bool $isLazy = false;
+
     protected function getStats(): array
     {
         $userId = Auth::id();
@@ -17,14 +19,19 @@ class StatsOverview extends BaseWidget
         if ($user && ($user->name === 'admin')) {
             // Admin gets all data
             return [
-                    Stat::make('Total Kendaraan', Kendaraan::count()),
+                    // Stat::make('Total Kendaraan', Kendaraan::count())->color('primary'),
                     Stat::make('Total Sudah Datang', Kendaraan::whereNot('tgl_masuk', NULL)->count()),
                     Stat::make('Total Belum Datang', Kendaraan::where('tgl_masuk', NULL)->count()),
-                    Stat::make('Total Balaikota', Kendaraan::where('lokasi', 'Balaikota Dalam')->count()),
-                    Stat::make('Total Jimerto', Kendaraan::where('lokasi', 'Jimerto')->count()),
-                    Stat::make('Total Siola Lantai 7', Kendaraan::where('lokasi', 'Siola Lantai 7')->count()),
-                    Stat::make('Total Siola Lantai 8', Kendaraan::where('lokasi', 'Siola Lantai 8')->count()),
-                    Stat::make('Total Convention Hall', Kendaraan::where('lokasi', 'Convention Hall')->count())
+                    Stat::make('Sudah Datang Balaikota', Kendaraan::where('lokasi', 'Balaikota Dalam')->whereNot('tgl_masuk', NULL)->count()),
+                    Stat::make('Belum Datang Balaikota', Kendaraan::where('lokasi', 'Balaikota Dalam')->where('tgl_masuk', NULL)->count()),
+                    Stat::make('Sudah Datang Jimerto', Kendaraan::where('lokasi', 'Jimerto')->whereNot('tgl_masuk', NULL)->count()),
+                    Stat::make('Belum Datang Jimerto', Kendaraan::where('lokasi', 'Jimerto')->where('tgl_masuk', NULL)->count()),
+                    Stat::make('Sudah Datang Lantai 7', Kendaraan::where('lokasi', 'Siola Lantai 7')->whereNot('tgl_masuk', NULL)->count()),
+                    Stat::make('Belum Datang Siola Lantai 7', Kendaraan::where('lokasi', 'Siola Lantai 7')->where('tgl_masuk', NULL)->count()),
+                    Stat::make('Sudah Datang Lantai 8', Kendaraan::where('lokasi', 'Siola Lantai 8')->whereNot('tgl_masuk', NULL)->count()),
+                    Stat::make('Belum Datang Siola Lantai 8', Kendaraan::where('lokasi', 'Siola Lantai 8')->where('tgl_masuk', NULL)->count()),
+                    Stat::make('Sudah Datang Convention Hall', Kendaraan::where('lokasi', 'Convention Hall')->whereNot('tgl_masuk', NULL)->count()),
+                    Stat::make('Belum Datang Convention Hall', Kendaraan::where('lokasi', 'Convention Hall')->where('tgl_masuk', NULL)->count())
             ];
         }
         else {
